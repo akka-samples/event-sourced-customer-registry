669aea17e7f60fdc712593e12883b1de2fdcfc6e
lightbend-tools-secure <lightbend-tools-secure@lightbend.com>
2025-03-17 12:26:15 +0000

    chore: bump SDK version to 3.2.2

--- src/main/java/customer/CustomerRegistrySetup.java
+++ src/main/java/customer/CustomerRegistrySetup.java
@@ -1,8 +1,10 @@
 package customer;
 
+import akka.javasdk.DependencyProvider;
 import akka.javasdk.JsonSupport;
 import akka.javasdk.ServiceSetup;
 import akka.javasdk.annotations.Setup;
+import customer.application.CustomerStore;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
@@ -12,6 +14,12 @@ import static com.fasterxml.jackson.databind.DeserializationFeature.FAIL_ON_NULL
 public class CustomerRegistrySetup implements ServiceSetup {
 
   private static final Logger logger = LoggerFactory.getLogger(CustomerRegistrySetup.class);
+
+  @Override
+  public DependencyProvider createDependencyProvider() {
+    return DependencyProvider.single(new CustomerStore());
+  }
+
 
 
   @Override
---
+++ src/main/java/customer/application/CustomerStore.java
@@ -0,0 +1,33 @@
+package customer.application;
+
+import akka.Done;
+import customer.domain.Customer;
+
+import java.util.Collection;
+import java.util.Map;
+import java.util.Optional;
+import java.util.concurrent.CompletionStage;
+import java.util.concurrent.ConcurrentHashMap;
+
+import static akka.Done.done;
+import static java.util.concurrent.CompletableFuture.completedFuture;
+
+/**
+ * A simple customer store implementation used for idempotent updates documentation.
+ */
+public class CustomerStore {
+
+  private Map<String, Customer> store = new ConcurrentHashMap<>();
+
+  public CompletionStage<Optional<Customer>> getById(String customerId) {
+    return completedFuture(Optional.ofNullable(store.get(customerId)));
+  }
+
+  public CompletionStage<Done> save(String customerId, Customer customer) {
+    return completedFuture(store.put(customerId, customer)).thenApply(__ -> done());
+  }
+
+  public CompletionStage<Collection<Customer>> getAll() {
+    return completedFuture(store.values());
+  }
+}
